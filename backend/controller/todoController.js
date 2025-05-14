aea335313229af2bd6137f21e044b20fcc1b5b1a
modsuhail <modsuhail45@gmail.com>
2025-05-14 12:03:06 +0530

    added delete and update controllers

--- backend/controller/todoController.js
+++ backend/controller/todoController.js
@@ -24,8 +24,6 @@ let getTodos = async (req, res) => {
 
         let todos = await Todos.find()
 
-        console.log(todos)
-
         res.json(todos)
 
     } catch (error) {
@@ -34,6 +32,41 @@ let getTodos = async (req, res) => {
 }
 
 
+const deleteTodo = async (req, res) => {
+    try {
+
+        const deletedTodo = await Todos.findByIdAndDelete(req.params.id)
+
+        if (!deletedTodo) {
+            return res.status(404).json({ message: 'Todo not founded' })
+        }
+
+        return res.json({ message: 'deleted' })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 
-export { createTodo, getTodos }
+const updateTodo = async (req, res) => {
+    try {
+
+        let { title, description, isCompleted } = req.body
+
+        let updatedTodo = await Todos.findByIdAndUpdate(req.query.id, { title, description, isCompleted })
+
+        if (!updatedTodo) {
+            return res.status(404).json({ message: 'Todo not founded' })
+        }
+
+        return res.json({ message: 'Todo updated successfully' })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+export { createTodo, getTodos, deleteTodo, updateTodo }
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import { createTodo, getTodos } from '../controller/todoController.js'
+import { createTodo, deleteTodo, getTodos, updateTodo } from '../controller/todoController.js'
 
 const todoRoute = express.Router()
 
 todoRoute.post('/', createTodo)
 
 todoRoute.get('/getTodos', getTodos)
+
+todoRoute.patch('/updatetodo', updateTodo)
+
+todoRoute.delete('/:id', deleteTodo)
 
 
 export default todoRoute
